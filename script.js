c06b9cc58f8e9b619fc085f5fed74b716eb8b149
loserchik67 <loserchik67@gmail.com>
2021-04-23 17:27:15 -0500

    Finish implementing the basic game logic

--- script.js
+++ script.js
@@ -9,11 +9,31 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
 // document.querySelector('.guess').value = 23;
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
-
 let score = 20;
+let highScore = 0;
+
+//replay reset changed items
+document.querySelector('.again').addEventListener('click', function () {
+  console.log('button clicked');
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = secretNumber;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  //remove background gradient and change colors back to originals
+  //document.querySelector('body').style.backgroundColor = '#222'   ended up just removing the gradient image instead of changing bg color
+  //kept that line in here to show that i can change this too;
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.again').style.color = '#222';
+  document.querySelector('.number').style.color = '#222';
+  document.querySelector('.check').style.color = '#222';
+  document.querySelector('body').style.backgroundImage = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,7 +45,18 @@ document.querySelector('.check').addEventListener('click', function () {
 
     //when player wins
   } else if (guess === secretNumber) {
+    //set highscore
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundImage =
+      'linear-gradient(to bottom right, #ff99ff, #00e6b8)';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.again').style.color = '#a366ff';
+    document.querySelector('.number').style.color = '#a366ff';
+    document.querySelector('.check').style.color = '#a366ff';
 
     //when guess is too low
   } else if (guess < secretNumber) {
