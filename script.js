0e5ae12aa92f181a6a48cae03e3e3f524f2fa274
loserchik67 <loserchik67@gmail.com>
2021-04-23 16:14:32 -0500

    Implement basic game logic

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
 'use strict';
+//example stuff
+// console.log(document.querySelector('.message').textContent);
 
-console.log(document.querySelector('.message'));
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 23;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Guessed Number!';
+
+    //when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+
+    //when guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //when guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
